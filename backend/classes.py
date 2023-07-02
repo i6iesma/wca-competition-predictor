f0ac8627e2fde4165457923eb94f81d8f3ea6cf4
i6iesma <inigo@escolar.net>
2023-07-02 19:06:08 +0200

    add all events kind of working

--- backend/classes.py
+++ backend/classes.py
@@ -4,9 +4,6 @@ class User:
     pb_single_333 = 0
     pb_avg_333 = 0
 
-    def __init__(self, name, wca_id, pb_single_333, pb_avg_333):
+    def __init__(self, name, wca_id ):
         self.name = name
         self.wca_id =wca_id
-        self.pb_single_333 = pb_single_333
-        self.pb_avg_333 = pb_avg_333
-        
--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,17 @@ connection = connector.connect(
     user = "inigo",
     password = "123456"
 )
-
 #Commands in order to use wca_dev db
 cursor = connection.cursor(buffered=True)
 cursor.execute("use wca_dev")
+#Get all of the events from the database
+cursor.execute("select id from Events;")
+events = []
+for event in list(cursor.fetchall()):
+    events.append(str(event)[2:][:-3])
+
+
+
 def get_all_users(competition_id):
     #Get all of the people ids on a give competition
     get_users_ids_query = "SELECT user_id FROM registrations WHERE competition_id ='" + competition_id + "' AND accepted_at IS NOT NULL AND deleted_at IS NULL;"
@@ -26,45 +33,44 @@ def get_all_users(competition_id):
 
         cursor.execute("select wca_id from users where id='" + str_id + "'")
         wca_id = str(cursor.fetchall())[3:][:-4]
-        #FIND 333 PB SINGLE
+        this_user = User(name, wca_id)
+        for event in events:
+            #Find single
+            cursor.execute("select best from RanksSingle where personId ='" + wca_id +  "' and eventId = '"+ event + "';")
+            exec("pb_single_" + event + " = str(cursor.fetchall())[2:][:-3]")
+            # #Some people may not have a result so this filters removing the entry completely
+            try:
+                exec("pb_single_" + event + "_int = int(pb_single_" + event + ")")
+            except ValueError:
+                #User does not have a result in this event
+                exec("if pb_single_"+ event + " == '': pb_single_" + event +  "= None")
+                continue
+                
+            exec("this_user.pb_single_" + event + " = pb_single_" + event + "_int") 
+            #Find avg 
+            cursor.execute("select best from RanksAverage where personId ='" + wca_id +  "' and eventId = '"+ event + "';")
+            exec("pb_avg_" + event + " = str(cursor.fetchall())[2:][:-3]")
+            # #Some people may not have a result so this filters removing the entry completely
+            try:
+                exec("pb_avg_" + event + "_int = int(pb_avg_" + event + ")")
+            except ValueError:
+                exec("if pb_avg_"+ event + " == '': pb_avg_" + event +  "= None")
+                continue
 
-        cursor.execute("select best from RanksSingle where personId ='" + wca_id +  "' and eventId = '333';")
-        pb_single_333 = str(cursor.fetchall())[2:][:-3]
-        #Some people may not have a result so this filters removing the entry completely
-        if pb_single_333 == '':
-            continue
-        pb_single_333_int = int(float(pb_single_333))
-
-        #FIND 333 PB AVG
-        cursor.execute("select best from RanksAverage where personId ='" + wca_id +  "' and eventId = '333';")
-        pb_avg_333 = str(cursor.fetchall())[2:][:-3]
-        #Some people may not have a result so this filters removing the entry completely
-        if pb_avg_333 == '':
-            continue
-        pb_single_333_int = int(float(pb_single_333))
-
-        pb_avg_333_int = int(pb_avg_333)
-   
-        #CREATE CLASS WITH INFO
-        users.append(User(name, wca_id, pb_single_333_int, pb_avg_333_int))
+            exec("this_user.pb_avg_" + event + " = pb_avg_" + event + "_int") 
+        # print(this_user.name, this_user.pb_single_333, this_user.pb_single_444)
+        users.append(this_user)
 
 
     return users
 
 def sort_users(event, format, users):
     #Sort all of the users based on their pb results on the event specified
-    if event == "333":
-        if format == "single":
-            users.sort(key=lambda user: user.pb_single_333)
-        if format == "avg":
-            users.sort(key=lambda user: user.pb_avg_333)
-
+    exec("users = [user for user in users if user.pb_" + format + "_" + event +" != 0]")
+    exec("users.sort(key=lambda user: user.pb_" + format + "_" + event + ")")
     return users
-def sort_criteria(user):
-    return user.pb_single_333
 
-users_sorted = sort_users("333", "avg", get_all_users('LazarilloOpen2023'))
-
-for user in users_sorted:
-    print(user.name, user.pb_single_333, user.pb_avg_333)
+sorted_users = sort_users("333", "avg", get_all_users("GetafeContinua2023"))
+for user in sorted_users:
+    print(user.name, user.pb_avg_333)
 
