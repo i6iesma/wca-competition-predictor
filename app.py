b2a659ce175869f97c68fbf227f7d9a3248f0013
i6iesma <inigo@escolar.net>
2023-07-15 17:05:51 +0200

    finished css styling

--- app.py
+++ app.py
@@ -4,9 +4,24 @@ sys.path.insert(1, './backend')
 import main as backend
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def index():
-    return render_template('index.html')
+    if request.method == "GET":
+        return render_template('index.html', users=[], event="", format="", len_users = 0)
+    elif request.method == "POST":
+        url = request.form.get('url')
+        format = request.form.get('format')
+        event = request.form.get('event')
+        print("event is " + str(event))
+        #Get the users from the backend
+        #Filter the competition id from url like this https://www.worldcubeassociation.org/competitions/LazarilloOpen2023
+        competition_id = str(url).strip("https://www.worldcubeassociation.org/competitions/")
+        #Get all the users already sorted
+        users = backend.main(competition_id, event, format)
+        len_users = len(users)
+        return render_template('index.html', users=users, event=event, format=format, len_users=len_users)
+
+        
 
 @app.route("/competition_ranking", methods=["POST", "GET"])
 def competition_ranking():
