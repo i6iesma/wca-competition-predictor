e085cb2b75eace380fd855ef7c70d00fabcc2e3a
Iñigo Escolar <inigo@MacBook-Pro-de-Inigo.local>
2023-08-04 16:25:49 +0200

    added update_db shell script without testing on server

---
+++ python-app/update_db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Download database from url
+rm databases/*.sql
+rm databases/*.zip
+echo "downloading database..."
+wget -P databases/ https://www.worldcubeassociation.org/wst/wca-developer-database-dump.zip 
+unzip databases/wca-developer-database-dump.zip -d databases
+rm wca-developer-database-dump.zip
+echo "download finished, starting import to database, this might take a while"
+mysql -u inigo -pinigo wca_dev < databases/wca-developer-database-dump.sql
+echo "import finished, exited successfully"
+
