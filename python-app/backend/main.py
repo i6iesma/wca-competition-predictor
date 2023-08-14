b65968125a571df134d117a3081cfbef65fcd7b4
i6iesma <inigo@escolar.net>
2023-08-14 18:46:17 +0200

    added average of the averages in the last competition working without external testing

--- python-app/backend/main.py
+++ python-app/backend/main.py
@@ -31,7 +31,41 @@ def get_all_users(competition_id,  cursor):
         }
         users.append(user)
     return users
-def get_pbs(users, event, format, cursor):
+
+def get_average_of_last_comp_averages(users, event, format, competitionId, cursor):
+    new_users = []
+    for user in users:
+        #First get the last competition the competitor competed in that event
+        sql_insert_query = "select competitionId from Results where personId=%s and eventId=%s order by id desc limit 1;" 
+        sql_parameters = (user["wca_id"], event)
+        cursor.execute(sql_insert_query, sql_parameters)
+        last_comp = str(cursor.fetchall())[3:][:-4]
+        if format == "single":
+            sql_insert_query = "select best from Results where personId='" + user["wca_id"] + "' and eventId=%s and competitionId=%s;"
+        if format == "avg":
+            sql_insert_query = "select average from Results where personId='" + user["wca_id"] + "' and eventId=%s and competitionId=%s;"
+
+
+        sql_parameters = (event, last_comp)
+        cursor.execute(sql_insert_query, sql_parameters)
+        averages = cursor.fetchall()
+        if averages == []:
+            continue
+        new_averages = []
+        for average in averages:
+            average = str(average)[1:][:-2]
+            new_averages.append(int(average))
+
+        #Find the average of the averages
+        if new_averages == [-1]:
+            continue
+        result = sum(new_averages) // len(new_averages)
+        user["result"] = str(result) 
+        if user["result"] != '':
+            new_users.append(user)
+    return new_users
+
+def get_pb(users, event, format, cursor):
     new_users = []
     # Find pb single at the event and format specified
     for user in users:
@@ -54,7 +88,6 @@ def get_pbs(users, event, format, cursor):
             }
             new_users.append(new_user)
 
-    debug_user_status(new_users, "get_pbs()")
     return new_users
 
             
@@ -68,7 +101,6 @@ def sort_users(users):
     for user in users:
         ranking = ranking + 1
         user["ranking"] = ranking
-    debug_user_status(users, "sort_users()")
     return users
 
 
@@ -86,7 +118,6 @@ def fix_centiseconds(users, event):
         seconds = int(cs_pb) / 100
         minutes = seconds // 60
         remainder_seconds = seconds - (60*minutes)
-        print(int(minutes))
         if int(minutes) == 0:
             # Sometimes python has a weird thing where it adds many decimals so this rounds it
             if len(str(remainder_seconds)) > 5:
@@ -95,7 +126,6 @@ def fix_centiseconds(users, event):
         else:
             final_str = str(int(minutes)) + ":" + str(remainder_seconds)
         user["result"] = final_str
-    debug_user_status(users, "fix_centiseconds()")
     return users
 
 
@@ -112,7 +142,6 @@ def multiblind_formatting(users, event):
         # This is an example of how data is showed 36/38 58:23
         # I only extract solved attempted and time in seconds becouse
         # is the only thing needed to show like in wca website
-        print("result is " + result)
         if result != '':
             difference = str(99 - int(result[:2]))
             time_in_seconds = str(result[2:][:-2])
@@ -128,8 +157,8 @@ def multiblind_formatting(users, event):
 def debug_user_status(users, previous_function):
     for user in users:
         print("this user has a result of " + user["result"] + " and this was called from " + previous_function)
-
-def main(competition_id, event, format, mode):
+#Competition id is written with camelCase because of the way it is in the wca db
+def main(competitionId, event, format, mode):
     # Connection with the db named wca_dev
     connection = connector.connect(
         host="localhost",
@@ -140,17 +169,13 @@ def main(competition_id, event, format, mode):
     cursor = connection.cursor(buffered=True)
     cursor.execute("use wca_dev")
     #Example code for getting user data in the pb mode
-    users = get_all_users(competition_id, cursor)
+    users = get_all_users(competitionId, cursor)
     if mode == "pb":
-        
-        users = get_pbs(users, event, format, cursor)
-
+        users = get_pb(users, event, format, cursor)
+    elif mode == "average_of_last_comp_averages":
+        users = get_average_of_last_comp_averages(users, event, format, competitionId, cursor)
     users = sort_users(users)
     users = fix_centiseconds(users, event)
     # Close the connection
     connection.close()
-    debug_user_status(users, "main()")
     return users
-
-
-print(main("LazarilloOpen2023", "333mbf", "single", "pb"))
